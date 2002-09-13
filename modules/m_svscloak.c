b1d4da87d29e61109907ef9a0cd6f421d30bdd97
Stuart Walsh <stu@oftc.net>
2002-09-13 02:32:33 +0000

    oops, missed the emptystring sanity check

--- modules/m_svscloak.c
+++ modules/m_svscloak.c
@@ -71,7 +71,7 @@ void m_svscloak(struct Client *client_p, struct Client *source_p, int parc, char
   if (!IsService(source_p))
     return;
 
-  if(parc < 3)
+  if(parc < 3 || EmptyString(parv[2]))
   {   
     sendto_one(source_p, form_str(ERR_NEEDMOREPARAMS), me.name, parv[0]);
     return;
