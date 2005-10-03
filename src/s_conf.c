58d48efa4411c6a6d429684d609cdc62ffc98628
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2005-10-03 12:13:38 +0000

    - Reverted oper_privs_as_string() back to its previous version which is easier   to maintain when adding new operflags. Even the one who wrote the new version   introduced a bug by not taking care to keep the oper_flags_table in sync   with the OPER_FLAGS_* bits.  (which also no one really wants to do ...) - Updated it to deal with new 'remoteban', 'operwall' and 'operspy' flags
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid-7.2@58 82007160-df01-0410-b94d-b575c5fd34c7

--- src/s_conf.c
+++ src/s_conf.c
@@ -2332,23 +2332,50 @@ expire_tklines(dlink_list *tklist)
 
 /* oper_privs_as_string()
  *
- * inputs        - pointer to client_p or NULL
+ * inputs        - pointer to client_p
  * output        - pointer to static string showing oper privs
  * side effects  - return as string, the oper privs as derived from port
  */
-
-static const unsigned int oper_flags_table[] = 
-  { 'O', 'R', 'U', 'G', 'N', 'K', 'X', 'D', 'H', 'A' , 0 };
+static const struct oper_privs
+{
+  const unsigned int oprivs;
+  const unsigned int hidden;
+  const unsigned char c;
+} flag_list[] = {
+  { OPER_FLAG_ADMIN,       OPER_FLAG_HIDDEN_ADMIN,  'A' },
+  { OPER_FLAG_REMOTEBAN,   0,                       'B' },
+  { OPER_FLAG_DIE,         0,                       'D' },
+  { OPER_FLAG_GLINE,       0,                       'G' },
+  { OPER_FLAG_REHASH,      0,                       'H' },
+  { OPER_FLAG_K,           0,                       'K' },
+  { OPER_FLAG_OPERWALL,    0,                       'L' },
+  { OPER_FLAG_N,           0,                       'N' },
+  { OPER_FLAG_GLOBAL_KILL, 0,                       'O' },
+  { OPER_FLAG_REMOTE,      0,                       'R' },
+  { OPER_FLAG_OPER_SPY,    0,                       'S' },
+  { OPER_FLAG_UNKLINE,     0,                       'U' },
+  { OPER_FLAG_X,           0,                       'X' },
+  { 0, 0, '\0' }
+};
 
 char *
 oper_privs_as_string(const unsigned int port)
 {
-  static char privs_out[12];
+  static char privs_out[16];
+  char *privs_ptr = privs_out;
+  unsigned int i = 0;
 
-  if (port & OPER_FLAG_HIDDEN_ADMIN)
-    flags_to_ascii(port & ~OPER_FLAG_ADMIN, oper_flags_table, privs_out, 1);
-  else
-    flags_to_ascii(port, oper_flags_table, privs_out, 1);
+  for (; flag_list[i].oprivs; ++i)
+  {
+    if ((port & flag_list[i].oprivs) &&
+        (port & flag_list[i].hidden) == 0)
+      *privs_ptr++ = flag_list[i].c;
+    else
+      *privs_ptr++ = ToLowerTab[flag_list[i].c];
+  }
+
+  *privs_ptr = '\0';
+
   return privs_out;
 }
 
