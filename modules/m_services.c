127df7fadd39f51a75d603b219b83ed4390b5e18
Stuart Walsh <stu@oftc.net>
2006-06-23 11:53:10 +0000

    Use m_services in contrib and get rid of all our individual services modules

--- modules/m_services.c
+++ modules/m_services.c
@@ -19,7 +19,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *  USA
  *
- *  $Id$
+ *  $Id: m_services.c 606 2006-06-08 22:35:55Z stu $
  */
 /*
  *
@@ -230,7 +230,7 @@ _moddeinit(void)
   mod_del_cmd(&os_msgtab);
 }
 
-const char *_version = "$Revision$";
+const char *_version = "$Revision: 606 $";
 #endif
 
 /*
