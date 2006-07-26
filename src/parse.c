f66649a217fa1b9ace1ef40247896588b4e2ca14
Stuart Walsh <stu@oftc.net>
2006-07-26 16:03:46 +0000

    fixed an old bug that affected our numeric delivery(shows up in ts6(origin)->ts6->ts6->ts5(target) scenarios

--- src/parse.c
+++ src/parse.c
@@ -823,10 +823,9 @@ do_numeric(char numeric[], struct Client *client_p, struct Client *source_p,
     if (ConfigServerHide.hide_servers &&
         MyClient(target_p) && !IsOper(target_p))
       sendto_one(target_p, ":%s %s %s%s", me.name, numeric, target_p->name, buffer);
-    else if (!MyClient(target_p) && IsCapable(target_p->from, CAP_TS6) && HasID(source_p))
-      sendto_one(target_p, ":%s %s %s%s", source_p->id, numeric, target_p->id, buffer);
-    else /* either it is our client, or a client linked throuh a non-ts6 server. must use names! */
-      sendto_one(target_p, ":%s %s %s%s", source_p->name, numeric, target_p->name, buffer);
+    else
+      sendto_one(target_p, ":%s %s %s%s", ID_or_name(source_p, target_p->from),
+                 numeric, ID_or_name(target_p, target_p->from), buffer);
     return;
   }
   else if ((chptr = hash_find_channel(parv[1])) != NULL)
