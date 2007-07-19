f2613b43dce6dcbc474b07b2cab8b0c7b9332072
Stuart Walsh <stu@oftc.net>
2007-07-19 15:26:47 +0000

    add the +R whois line back again Fixes #82

--- modules/m_whois.c
+++ modules/m_whois.c
@@ -420,6 +420,10 @@ whois_person(struct Client *source_p, struct Client *target_p)
                me.name, source_p->name, target_p->name,
                target_p->away);
 
+  if (target_p->umodes & UMODE_NICKSERVREG)
+    sendto_one(source_p, form_str(RPL_WHOISNICKSERVREG), me.name,
+        source_p->name, target_p->name);
+
   if (IsSetCallerId(target_p) && !IsSoftCallerId(target_p))
     sendto_one(source_p, form_str(RPL_TARGUMODEG),
                me.name, source_p->name, target_p->name);
