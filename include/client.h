bd8d0888352a56d926d21fdd6eab9f253381f513
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2007-11-01 12:04:58 +0000

    - on_allow_list and allow_list are no longer needed. Removed
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid-7.2@888 82007160-df01-0410-b94d-b575c5fd34c7

--- include/client.h
+++ include/client.h
@@ -155,14 +155,7 @@ struct Client
    */
   char              sockhost[HOSTIPLEN + 1]; /* This is the host name from the 
                                                 socket ip address as string */
-  /* caller ID allow list */
-  /* This has to be here, since a client on an on_allow_list could
-   * be a remote client. simpler to keep both here.
-   */
-  dlink_list	allow_list;	/* clients I'll allow to talk to me */
-  dlink_list	on_allow_list;	/* clients that have =me= on their allow list*/
-
-  dlink_list     channel;   /* chain of channel pointer blocks */
+  dlink_list        channel;   /* chain of channel pointer blocks */
 
   struct LocalUser *localClient;
 };
