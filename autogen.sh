7588d863423fd48daa8e487c9c64741bd12fab88
stu <stu@82007160-df01-0410-b94d-b575c5fd34c7>
2007-11-04 23:50:36 +0000

    Add workaround for libltdl being broken in debian sid
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid-7.2@910 82007160-df01-0410-b94d-b575c5fd34c7

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,8 @@ set -e
 aclocal-1.9
 autoheader
 libtoolize --automake --ltdl
+# Don't ask, but this is a workaround for libltdl in Debian unstable
+sed -i -e 's/install-sh ltmain.sh missing mkinstalldirs/install-sh ltmain.sh missing/' libltdl/Makefile.in
 automake-1.9 --add-missing
 autoconf
 ./configure "$@"
