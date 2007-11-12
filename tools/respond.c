b75031913f44c7a683ef8539ba2b6ae79a767f60
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2007-11-12 16:24:53 +0000

    - Made tools/ compile
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid-7.2@921 82007160-df01-0410-b94d-b575c5fd34c7

--- tools/respond.c
+++ tools/respond.c
@@ -17,7 +17,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *  $Id$
+ *  $Id: respond.c 33 2005-10-02 20:50:00Z knight $
  */
 #include <stdio.h>
 #include <string.h>
