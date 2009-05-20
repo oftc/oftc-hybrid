6c89622c019e1a86a372238d99edaac4b19faddc
Stuart Walsh <stu@oftc.net>
2009-05-20 10:27:21 +0000

    fix the comm_select timeout stuff, it was never used, therefore didnt work.  Also add a timeout for ssl handshakes, and disconnect if they are not completed quick enough.  Fixes #106.

--- src/s_bsd.c
+++ src/s_bsd.c
@@ -310,22 +310,29 @@ ssl_handshake(int fd, struct Client *client_p)
   }
 
   if (ret <= 0)
+  {
+    if((CurrentTime - client_p->firsttime) > 30)
+    {
+      exit_client(client_p, client_p, "Timeout during SSL handshake");
+      return;
+    }
     switch (SSL_get_error(client_p->localClient->fd.ssl, ret))
     {
       case SSL_ERROR_WANT_WRITE:
         comm_setselect(&client_p->localClient->fd, COMM_SELECT_WRITE,
-	               (PF *) ssl_handshake, client_p, 0);
+	               (PF *) ssl_handshake, client_p, 30);
         return;
 
       case SSL_ERROR_WANT_READ:
         comm_setselect(&client_p->localClient->fd, COMM_SELECT_READ,
-	               (PF *) ssl_handshake, client_p, 0);
+	               (PF *) ssl_handshake, client_p, 30);
         return;
 
       default:
         exit_client(client_p, client_p, "Error during SSL handshake");
         return;
     }
+  }
 
   execute_callback(auth_cb, client_p);
 }
