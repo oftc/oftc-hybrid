7fb3a48d8c5aa4d186d7ba57311cff440419f746
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2009-07-27 00:34:48 +0000

    - removed LL leftover in set_default_conf(). now ServerInfo.hub   properly initializes on /rehash
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid-7.2@956 82007160-df01-0410-b94d-b575c5fd34c7

--- src/s_conf.c
+++ src/s_conf.c
@@ -1822,8 +1822,9 @@ set_default_conf(void)
   ServerInfo.specific_ipv6_vhost = 0;
 
   ServerInfo.max_clients = MAXCLIENTS_MAX;
-  /* Don't reset hub, as that will break lazylinks */
-  /* ServerInfo.hub = NO; */
+
+  ServerInfo.hub = 0;
+  delete_capability("HUB");
   ServerInfo.dns_host.sin_addr.s_addr = 0;
   ServerInfo.dns_host.sin_port = 0;
   AdminInfo.name = NULL;
