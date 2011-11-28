e080cae9bd4ab349fe50320679b74317c828a00d
Timothy J Fontaine <tjfontaine@oftc.net>
2011-11-28 15:19:21 +0000

    simplify src/listener.c IsWebsocket checks

--- src/listener.c
+++ src/listener.c
@@ -149,35 +149,12 @@ inetport(struct Listener *listener)
   struct irc_ssaddr lsin;
   socklen_t opt = 1;
 
-  /*
-   * At first, open a new socket
-   */
-  if (!IsWebsocket(listener) && comm_open(&listener->fd, listener->addr.ss.ss_family, SOCK_STREAM, 0,
-                "Listener socket") == -1)
-  {
-    report_error(L_ALL, "opening listener socket %s:%s",
-                 get_listener_name(listener), errno);
-    return 0;
-  }
-
   memset(&lsin, 0, sizeof(lsin));
   memcpy(&lsin, &listener->addr, sizeof(struct irc_ssaddr));
-  
+
   getnameinfo((struct sockaddr*)&lsin, lsin.ss_len, listener->vhost, 
         HOSTLEN, NULL, 0, NI_NUMERICHOST);
   listener->name = listener->vhost;
-
-  /*
-   * XXX - we don't want to do all this crap for a listener
-   * set_sock_opts(listener);
-   */
-  if (!IsWebsocket(listener) && setsockopt(listener->fd.fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
-  {
-    report_error(L_ALL, "setting SO_REUSEADDR for listener %s:%s",
-                 get_listener_name(listener), errno);
-    fd_close(&listener->fd);
-    return 0;
-  }
 
   /*
    * Bind a port to listen for new connections if port is non-null,
@@ -185,7 +162,33 @@ inetport(struct Listener *listener)
    */
   lsin.ss_port = htons(listener->port);
 
-  if (!IsWebsocket(listener) && bind(listener->fd.fd, (struct sockaddr *)&lsin, lsin.ss_len))
+  if (IsWebsocket(listener))
+    return 1;
+
+  /*
+   * At first, open a new socket
+   */
+  if (comm_open(&listener->fd, listener->addr.ss.ss_family, SOCK_STREAM, 0,
+                "Listener socket") == -1)
+  {
+    report_error(L_ALL, "opening listener socket %s:%s",
+                 get_listener_name(listener), errno);
+    return 0;
+  }
+
+  /*
+   * XXX - we don't want to do all this crap for a listener
+   * set_sock_opts(listener);
+   */
+  if (setsockopt(listener->fd.fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
+  {
+    report_error(L_ALL, "setting SO_REUSEADDR for listener %s:%s",
+                 get_listener_name(listener), errno);
+    fd_close(&listener->fd);
+    return 0;
+  }
+
+  if (bind(listener->fd.fd, (struct sockaddr *)&lsin, lsin.ss_len))
   {
     report_error(L_ALL, "binding listener socket %s:%s",
                  get_listener_name(listener), errno);
@@ -193,7 +196,7 @@ inetport(struct Listener *listener)
     return 0;
   }
 
-  if (!IsWebsocket(listener) && listen(listener->fd.fd, HYBRID_SOMAXCONN))
+  if (listen(listener->fd.fd, HYBRID_SOMAXCONN))
   {
     report_error(L_ALL, "listen failed for %s:%s",
                  get_listener_name(listener), errno);
@@ -202,7 +205,6 @@ inetport(struct Listener *listener)
   }
 
   /* Listen completion events are READ events .. */
-
   accept_connection(&listener->fd, listener);
   return 1;
 }
