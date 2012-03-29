41f6205540804f13d4c6559d7fbecf5719ed6685
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2012-03-29 11:15:26 +0000

    - s_serv.h: sort out unused CAPABS, remove unused capabtab[]
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid-8@1320 82007160-df01-0410-b94d-b575c5fd34c7

--- include/s_serv.h
+++ include/s_serv.h
@@ -43,31 +43,29 @@ struct Channel;
 struct Capability
 {
   dlink_node node;
-  char *name; 	    /* name of capability */
+  char *name;       /* name of capability */
   unsigned int cap; /* mask value         */
 };
 
-#define CAP_CAP		0x00000001 /* received a CAP to begin with        */
-#define CAP_QS		0x00000002 /* Can handle quit storm removal       */
-#define CAP_EX		0x00000004 /* Can do channel +e exemptions        */
-#define CAP_CHW		0x00000008 /* Can do channel wall @#              */
-#define CAP_IE		0x00000010 /* Can do invite exceptions            */
-#define CAP_EOB		0x00000020 /* Can do EOB message                  */
-#define CAP_KLN		0x00000040 /* Can do KLINE message                */
-#define CAP_GLN		0x00000080 /* Can do GLINE message                */
-#define CAP_TS6		0x00000100 /* Can do TS6                          */
-#define __CAP_UNUSED__	0x00000200 /* UNUSED                              */
-#define __CAP_UNUSED___	0x00000400 /* UNUSED                              */
-#define CAP_KNOCK	0x00000800 /* supports KNOCK                      */
-#define CAP_TB	        0x00001000 /* supports TB                         */
-#define CAP_UNKLN	0x00002000 /* Can do UNKLINE message		  */
-#define CAP_CLUSTER	0x00004000 /* supports server clustering	  */
-#define CAP_ENCAP	0x00008000 /* supports ENCAP message		  */
-#define CAP_HOPS	0x00010000 /* supports HALFOPS			  */
-#define CAP_TBURST      0x00020000 /* supports TBURST                     */
-#define CAP_SVS         0x00040000 /* supports services                   */
-#define CAP_DLN         0x00080000 /* Can do DLINE message                */
-#define CAP_UNDLN       0x00100000 /* Can do UNDLINE message               */
+#define CAP_CAP         0x00000001 /* received a CAP to begin with        */
+#define CAP_QS          0x00000002 /* Can handle quit storm removal       */
+#define CAP_EX          0x00000004 /* Can do channel +e exemptions        */
+#define CAP_CHW         0x00000008 /* Can do channel wall @#              */
+#define CAP_IE          0x00000010 /* Can do invite exceptions            */
+#define CAP_EOB         0x00000020 /* Can do EOB message                  */
+#define CAP_KLN         0x00000040 /* Can do KLINE message                */
+#define CAP_GLN         0x00000080 /* Can do GLINE message                */
+#define CAP_TS6         0x00000100 /* Can do TS6                          */
+#define CAP_KNOCK       0x00000200 /* supports KNOCK                      */
+#define CAP_TB          0x00000400 /* supports TB                         */
+#define CAP_UNKLN       0x00000800 /* Can do UNKLINE message              */
+#define CAP_CLUSTER     0x00001000 /* supports server clustering          */
+#define CAP_ENCAP       0x00002000 /* supports ENCAP message              */
+#define CAP_HOPS        0x00004000 /* supports HALFOPS                    */
+#define CAP_TBURST      0x00008000 /* supports TBURST                     */
+#define CAP_SVS         0x00010000 /* supports services                   */
+#define CAP_DLN         0x00020000 /* Can do DLINE message                */
+#define CAP_UNDLN       0x00040000 /* Can do UNDLINE message              */
 
 /*
  * Capability macros.
@@ -76,15 +74,6 @@ struct Capability
 #define SetCapable(x, cap)      ((x)->localClient->caps |=  (cap))
 #define ClearCap(x, cap)        ((x)->localClient->caps &= ~(cap))
 
-/*
- * Globals
- *
- *
- * list of recognized server capabilities.  "TS" is not on the list
- * because all servers that we talk to already do TS, and the kludged
- * extra argument to "PASS" takes care of checking that.  -orabidoo
- */
-extern struct Capability captab[];
 
 /*
  * return values for hunt_server() 
