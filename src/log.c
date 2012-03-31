852b10c432af2e05f11c93b0c0281b3291c78656
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2012-03-31 17:18:22 +0000

    - minor cleanups to the new logging system
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid-8@1327 82007160-df01-0410-b94d-b575c5fd34c7

--- src/log.c
+++ src/log.c
@@ -28,7 +28,6 @@
 #include "irc_string.h"
 #include "ircd.h"
 #include "conf.h"
-#include "send.h"
 #include "s_misc.h"
 
 
@@ -80,18 +79,14 @@ log_exceed_size(unsigned int type)
   return (size_t)sb.st_size > log_type_table[type].size;
 }
 
-
 static void 
-write_log(enum log_type type, const char *message)
+log_write(enum log_type type, const char *message)
 {
-  char buf[LOG_BUFSIZE];
-  size_t nbytes = 0;
-  struct tm *lt = localtime(&CurrentTime);
+  char buf[IRCD_BUFSIZE];
 
-  nbytes = strftime(buf, sizeof(buf), "[%FT%H:%M:%S%z] ", lt);
-  snprintf(buf+nbytes, sizeof(buf)-nbytes, "%s\n", message);
+  strftime(buf, sizeof(buf), "[%FT%H:%M:%S%z]", localtime(&CurrentTime));
 
-  fputs(buf, log_type_table[type].file);
+  fprintf(log_type_table[type].file, "%s %s\n", buf, message);
   fflush(log_type_table[type].file);
 }
 
@@ -101,29 +96,28 @@ ilog(enum log_type type, const char *fmt, ...)
   char buf[LOG_BUFSIZE];
   va_list args;
 
-  if (log_type_table[type].file == NULL)
+  if (!log_type_table[type].file || !ConfigLoggingEntry.use_logging)
     return;
 
   va_start(args, fmt);
   vsnprintf(buf, sizeof(buf), fmt, args);
   va_end(args);
 
-  if (ConfigLoggingEntry.use_logging)
-  {
-    write_log(type, buf);
+  log_write(type, buf);
 
-    if (log_exceed_size(type) <= 0)
-      return;
+  if (log_exceed_size(type) <= 0)
+    return;
 
-    snprintf(buf, sizeof(buf), "Rotating logfile %s",
-             log_type_table[type].path);
-    write_log(type, buf);
-    fclose(log_type_table[type].file);
-    log_type_table[type].file = NULL;
+  snprintf(buf, sizeof(buf), "Rotating logfile %s",
+           log_type_table[type].path);
+  log_write(type, buf);
 
-    snprintf(buf, sizeof(buf), "%s.old", log_type_table[type].path);
-    unlink(buf);
-    rename(log_type_table[type].path, buf);
-    log_add_file(type, log_type_table[type].size, log_type_table[type].path);
-  }
+  fclose(log_type_table[type].file);
+  log_type_table[type].file = NULL;
+
+  snprintf(buf, sizeof(buf), "%s.old", log_type_table[type].path);
+  unlink(buf);
+  rename(log_type_table[type].path, buf);
+
+  log_add_file(type, log_type_table[type].size, log_type_table[type].path);
 }
