600edf9100c492a51013e8ff6f2b3fb22a930382
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2012-05-01 11:25:58 +0000

    - m_server.c: fixed core introduced by previous commit to m_server.c
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid-8@1384 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/core/m_server.c
+++ modules/core/m_server.c
@@ -293,7 +293,6 @@ ms_server(struct Client *client_p, struct Client *source_p,
     if (target_p != client_p)
       exit_client(target_p, &me, "Overridden");
 
-//  ptr = client_p->confs.head;
   aconf = map_to_conf(source_p->localClient->confs.head->data);
 
   /* See if the newly found server is behind a guaranteed
@@ -484,6 +483,8 @@ ms_sid(struct Client *client_p, struct Client *source_p,
   if ((target_p = find_servconn_in_progress(parv[1])))
     if (target_p != client_p)
       exit_client(target_p, &me, "Overridden");
+
+  aconf = map_to_conf(source_p->localClient->confs.head->data);
 
   /* See if the newly found server is behind a guaranteed
    * leaf. If so, close the link.
