ab0fdf31be9c94b3ed12945384d752f12a839cb9
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2012-07-21 20:11:48 +0000

    - s_user.c:oper_up(): information about oper privileges can be obtained via   "STATS p|o". No need to send a separate notice on oper-up.
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid-8@1469 82007160-df01-0410-b94d-b575c5fd34c7

--- src/s_user.c
+++ src/s_user.c
@@ -1202,8 +1202,7 @@ check_xline(struct Client *source_p)
 void
 oper_up(struct Client *source_p)
 {
-  unsigned int old = source_p->umodes;
-  const char *operprivs = "";
+  const unsigned int old = source_p->umodes;
   const struct AccessItem *oconf = NULL;
 
   assert(source_p->localClient->confs.head);
@@ -1225,8 +1224,6 @@ oper_up(struct Client *source_p)
   assert(dlinkFind(&oper_list, source_p) == NULL);
   dlinkAdd(source_p, make_dlink_node(), &oper_list);
 
-  operprivs = oper_privs_as_string(oconf->port);
-
   AddOFlag(source_p, oconf->port);
 
   if (HasOFlag(source_p, OPER_FLAG_ADMIN))
@@ -1238,8 +1235,6 @@ oper_up(struct Client *source_p)
                        get_oper_name(source_p));
   send_umode_out(source_p, source_p, old);
   sendto_one(source_p, form_str(RPL_YOUREOPER), me.name, source_p->name);
-  sendto_one(source_p, ":%s NOTICE %s :*** Oper privs are %s",
-             me.name, source_p->name, operprivs);
 }
 
 static char new_uid[TOTALSIDUID + 1];     /* allow for \0 */
