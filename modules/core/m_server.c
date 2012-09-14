5fa1377cd629ce984e00f740fc639d2d3e11d7dd
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2012-09-14 18:20:34 +0000

    - ms_sid, ms_server: minor optimization. if we already found a matching   hub/leaf mask, don't continue looking for other masks
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid-8@1528 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/core/m_server.c
+++ modules/core/m_server.c
@@ -286,11 +286,11 @@ ms_server(struct Client *client_p, struct Client *source_p,
    */
   DLINK_FOREACH(ptr, aconf->leaf_list.head)
     if (match(ptr->data, name))
-      ++llined;
+      llined = 1, break;
 
   DLINK_FOREACH(ptr, aconf->hub_list.head)
     if (match(ptr->data, name))
-      ++hlined;
+      hlined = 1, break;
 
   /* Ok, this way this works is
    *
@@ -477,11 +477,11 @@ ms_sid(struct Client *client_p, struct Client *source_p,
    */
   DLINK_FOREACH(ptr, aconf->leaf_list.head)
     if (match(ptr->data, parv[1]))
-      ++llined;
+      llined = 1, break;
 
   DLINK_FOREACH(ptr, aconf->hub_list.head)
     if (match(ptr->data, parv[1]))
-      ++hlined;
+      hlined = 1, break;
 
 
   /* Ok, this way this works is
