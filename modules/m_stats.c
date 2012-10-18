7a26337b4eef16ecda0090fa8a5d75de078fc292
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2012-10-18 14:41:13 +0000

    - Fixed bug in stats_klines() showing oper_reason to unopered clients - Fixed /stats d|D showing "(null)" if there's no oper_reason
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid-8@1576 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/m_stats.c
+++ modules/m_stats.c
@@ -417,7 +417,7 @@ stats_deny(struct Client *source_p, int parc, char *parv[])
 
         sendto_one(source_p, form_str(RPL_STATSDLINE),
                    from, to, 'D', aconf->host, aconf->reason,
-		   aconf->oper_reason);
+		   aconf->oper_reason ? aconf->oper_reason : "");
       }
     }
   }
@@ -456,7 +456,7 @@ stats_tdeny(struct Client *source_p, int parc, char *parv[])
 
         sendto_one(source_p, form_str(RPL_STATSDLINE),
                    from, to, 'd', aconf->host, aconf->reason,
-		   aconf->oper_reason);
+		   aconf->oper_reason ? aconf->oper_reason : "");
       }
     }
   }
@@ -498,7 +498,7 @@ stats_exempt(struct Client *source_p, int parc, char *parv[])
 
         sendto_one(source_p, form_str(RPL_STATSDLINE),
                    from, to, 'e', aconf->host, 
-		   aconf->reason, aconf->oper_reason);
+		   aconf->reason, aconf->oper_reason ? aconf->oper_reason : "");
       }
     }
   }
@@ -896,8 +896,7 @@ stats_klines(struct Client *source_p, int parc, char *parv[])
       return;
       
     sendto_one(source_p, form_str(RPL_STATSKLINE), from,
-               to, "K", aconf->host, aconf->user, aconf->reason,
-	       aconf->oper_reason);
+               to, "K", aconf->host, aconf->user, aconf->reason, "");
   }
   /* Theyre opered, or allowed to see all klines */
   else {
