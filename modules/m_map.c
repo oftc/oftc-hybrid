pcangel/ircd-hybrid

- Got rid of Serv.dep_users and Serv.dep_servers
git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid-7.2@889 82007160-df01-0410-b94d-b575c5fd34c7

modules/m_map.c
@@ -33,22 +33,26 @@
 #include "irc_string.h"
 #include "sprintf_irc.h"
 
+
+static char buf[IRCD_BUFSIZE];
 static void m_map(struct Client *, struct Client *, int, char *[]);
 static void mo_map(struct Client *, struct Client *, int, char *[]);
 static void dump_map(struct Client *, struct Client *, int, char *);
 
 struct Message map_msgtab = {
   "MAP", 0, 0, 0, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_map, m_ignore, m_ignore, mo_map, m_ignore}
+  { m_unregistered, m_map, m_ignore, m_ignore, mo_map, m_ignore }
 };
 
 #ifndef STATIC_MODULES
-void _modinit(void)
+void
+_modinit(void)
 {
   mod_add_cmd(&map_msgtab);
 }
 
-void _moddeinit(void)
+void
+_moddeinit(void)
 {
   mod_del_cmd(&map_msgtab);
 }
@@ -56,7 +60,6 @@ void _moddeinit(void)
 const char *_version = "$Revision$";
 #endif
 
-static char buf[IRCD_BUFSIZE];
 
 /* m_map()
  *	parv[0] = sender prefix
@@ -75,11 +78,11 @@ m_map(struct Client *client_p, struct Client *source_p,
       {
         /* safe enough to give this on a local connect only */
         sendto_one(source_p, form_str(RPL_LOAD2HI),
-          me.name, source_p->name);
+                   me.name, source_p->name);
         return;
       }
-      else
-        last_used = CurrentTime;
+
+      last_used = CurrentTime;
     }
 
     dump_map(client_p, &me, 0, buf);
@@ -106,12 +109,11 @@ mo_map(struct Client *client_p, struct Client *source_p,
  */
 static void
 dump_map(struct Client *client_p, struct Client *root_p, int start_len,
-	 char *pbuf)
+         char *pbuf)
 {
   int cnt = 0, i = 0, l = 0, len = start_len;
   int users, dashes;
   dlink_node *ptr;
-  struct Client *server_p;
   char *pb;
 
   *pbuf= '\0';
@@ -121,37 +123,33 @@ dump_map(struct Client *client_p, struct Client *root_p, int start_len,
   pb += l;
   len += l;
 
-  /* IsOper isn't called *that* often. */
-  if (IsOper(client_p))
+  if (root_p->id[0] != '\0')
   {
-    if (root_p->id[0] != '\0')
-    {
-      l = ircsprintf(pb, "[%s]", root_p->id);
-      pb += l;
-      len += l;
-    }
+    l = ircsprintf(pb, "[%s]", root_p->id);
+    pb += l;
+    len += l;
   }
 
   *pb++ = ' ';
   len++;
   dashes = 50 - len;
-  for(i = 0; i < dashes; i++)
-  {
+
+  for (i = 0; i < dashes; i++)
     *pb++ = '-';
-  }
+
   *pb++ = ' ';
   *pb++ = '|';
 
-  users = dlink_list_length(&root_p->serv->users);
+  users = dlink_list_length(&root_p->serv->client_list);
 
   sprintf(pb, " Users: %5d (%1.1f%%)", users,
-	  100 * (float)users / (float)Count.total);
+          100 * (float)users / (float)Count.total);
 
   sendto_one(client_p, form_str(RPL_MAP), me.name, client_p->name, buf);
-        
-  if (root_p->serv->servers.head)
+
+  if (root_p->serv->server_list.head)
   {
-    cnt += dlink_list_length(&root_p->serv->servers);
+    cnt += dlink_list_length(&root_p->serv->server_list);
 
     if (cnt)
     {
@@ -167,19 +165,20 @@ dump_map(struct Client *client_p, struct Client *root_p, int start_len,
 
   i = 1;
 
-  DLINK_FOREACH(ptr, root_p->serv->servers.head)
+  DLINK_FOREACH(ptr, root_p->serv->server_list.head)
   {
-    server_p = ptr->data;
+    struct Client *server_p = ptr->data;
 
     *pbuf = ' ';
+
     if (i < cnt)
       *(pbuf + 1) = '|';
     else
       *(pbuf + 1) = '`';
-      
+
     *(pbuf + 2) = '-';
     *(pbuf + 3) = ' ';
-    dump_map(client_p, server_p, start_len+4, pbuf+4);
+    dump_map(client_p, server_p, start_len + 4, pbuf + 4);
  
     ++i;
   }

oftc/oftc-hybrid

modules/m_map.c
@@ -34,21 +34,25 @@
 #include "irc_string.h"
 #include "sprintf_irc.h"
 
+
+static char buf[IRCD_BUFSIZE];
 static void mo_map(struct Client *, struct Client *, int, char *[]);
 static void dump_map(struct Client *, struct Client *, int, char *);
 
 struct Message map_msgtab = {
   "MAP", 0, 0, 0, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_not_oper, m_ignore, m_ignore, mo_map, m_ignore}
+  { m_unregistered, m_not_oper, m_ignore, m_ignore, mo_map, m_ignore }
 };
 
 #ifndef STATIC_MODULES
-void _modinit(void)
+void
+_modinit(void)
 {
   mod_add_cmd(&map_msgtab);
 }
 
-void _moddeinit(void)
+void
+_moddeinit(void)
 {
   mod_del_cmd(&map_msgtab);
 }
@@ -56,7 +60,6 @@ void _moddeinit(void)
 const char *_version = "$Revision$";
 #endif
 
-static char buf[IRCD_BUFSIZE];
 static int line_counter;
 
 /* mo_map()
@@ -96,12 +99,11 @@ mo_map(struct Client *client_p, struct Client *source_p,
  */
 static void
 dump_map(struct Client *client_p, struct Client *root_p, int start_len,
-	 char *pbuf)
+         char *pbuf)
 {
   int cnt = 0, i = 0, l = 0, len = start_len;
   int users, dashes;
   dlink_node *ptr;
-  struct Client *server_p;
   char *pb;
   int print_dashes;
 
@@ -118,12 +120,9 @@ dump_map(struct Client *client_p, struct Client *root_p, int start_len,
   /* IsOper isn't called *that* often. */
   if (IsOper(client_p))
   {
-    if (root_p->id[0] != '\0')
-    {
-      l = ircsprintf(pb, "[%s]", root_p->id);
-      pb += l;
-      len += l;
-    }
+    l = ircsprintf(pb, "[%s]", root_p->id);
+    pb += l;
+    len += l;
   }
 
   *pb++ = ' ';
@@ -134,7 +133,7 @@ dump_map(struct Client *client_p, struct Client *root_p, int start_len,
     *pb++ = print_dashes ? '-' : ' ';
   }
 
-  users = dlink_list_length(&root_p->serv->users);
+  users = dlink_list_length(&root_p->serv->client_list);
 
   sprintf(pb, "%5d [%4.1f%%]", users,
 	  100 * (float)users / (float)Count.total);
@@ -142,10 +141,10 @@ dump_map(struct Client *client_p, struct Client *root_p, int start_len,
       while(*(pb+1) == ' ') *pb++ = '-';
 
   sendto_one(client_p, form_str(RPL_MAP), me.name, client_p->name, buf);
-        
-  if (root_p->serv->servers.head)
+
+  if (root_p->serv->server_list.head)
   {
-    cnt += dlink_list_length(&root_p->serv->servers);
+    cnt += dlink_list_length(&root_p->serv->server_list);
 
     if (cnt)
     {
@@ -161,19 +160,20 @@ dump_map(struct Client *client_p, struct Client *root_p, int start_len,
 
   i = 1;
 
-  DLINK_FOREACH(ptr, root_p->serv->servers.head)
+  DLINK_FOREACH(ptr, root_p->serv->server_list.head)
   {
-    server_p = ptr->data;
+    struct Client *server_p = ptr->data;
 
     *pbuf = ' ';
+
     if (i < cnt)
       *(pbuf + 1) = '|';
     else
       *(pbuf + 1) = '`';
-      
+
     *(pbuf + 2) = '-';
     *(pbuf + 3) = ' ';
-    dump_map(client_p, server_p, start_len+4, pbuf+4);
+    dump_map(client_p, server_p, start_len + 4, pbuf + 4);
  
     ++i;
   }