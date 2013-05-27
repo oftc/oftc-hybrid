pcangel/ircd-hybrid

- Update CHANMODES ISUPORT...
git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid-7.3@1153 82007160-df01-0410-b94d-b575c5fd34c7

src/s_conf.c
@@ -2458,7 +2458,7 @@ read_conf_files(int cold)
   add_isupport("CHANLIMIT", chanlimit, -1);
   snprintf(chanmodes, sizeof(chanmodes), "%s%s%s",
            ConfigChannel.use_except ? "e" : "",
-	   ConfigChannel.use_invex ? "I" : "", "b,k,l,imnpst");
+	   ConfigChannel.use_invex ? "I" : "", "b,k,l,imnpstOS");
   add_isupport("CHANNELLEN", NULL, LOCAL_CHANNELLEN);
 
   if (ConfigChannel.use_except)

oftc/oftc-hybrid

src/s_conf.c
@@ -2602,7 +2602,7 @@ read_conf_files(int cold)
   add_isupport("CHANLIMIT", chanlimit, -1);
   snprintf(chanmodes, sizeof(chanmodes), "%s%s%s%s", 
       ConfigChannel.use_except ? "e" : "", ConfigChannel.use_invex ? "I" : "", 
-      ConfigChannel.use_quiet ? "q" : "", "b,k,l,imnpstMRS");
+      ConfigChannel.use_quiet ? "q" : "", "b,k,l,imnpstMORS");
   add_isupport("CHANNELLEN", NULL, LOCAL_CHANNELLEN);
 
   if (ConfigChannel.use_except)