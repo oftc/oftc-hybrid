5583a7080ca1e5c48d43a42cd52fcf4a8a9f5afa
Stuart Walsh <stu@ipng.org.uk>
2013-05-29 14:53:00 +0100

    fix challenge module

--- modules/m_challenge.c
+++ modules/m_challenge.c
@@ -57,6 +57,99 @@ failed_challenge_notice(struct Client *source_p, const char *name,
                          source_p->username, source_p->host, reason);
 }
 
+/*
+ * m_challenge - generate RSA challenge for wouldbe oper
+ * parv[0] = sender prefix
+ * parv[1] = operator to challenge for, or +response
+ *
+ */
+static void
+m_challenge(struct Client *client_p, struct Client *source_p,
+            int parc, char *parv[])
+{
+  char *challenge = NULL;
+  struct ConfItem *conf = NULL;
+  struct AccessItem *aconf = NULL;
+
+  /* if theyre an oper, reprint oper motd and ignore */
+  if (HasUMode(source_p, UMODE_OPER))
+  {
+    sendto_one(source_p, form_str(RPL_YOUREOPER), me.name, source_p->name);
+    send_message_file(source_p, &ConfigFileEntry.opermotd);
+    return;
+  }
+
+  if (*parv[1] == '+')
+  {
+    /* Ignore it if we aren't expecting this... -A1kmm */
+    if (source_p->localClient->response == NULL)
+      return;
+
+    if (irccmp(source_p->localClient->response, ++parv[1]))
+    {
+      sendto_one(source_p, form_str(ERR_PASSWDMISMATCH), me.name,
+                 source_p->name);
+      failed_challenge_notice(source_p, source_p->localClient->auth_oper,
+                              "challenge failed");
+      return;
+    }
+    
+    conf = find_exact_name_conf(OPER_TYPE,
+                                source_p->localClient->auth_oper,
+                                source_p->username, source_p->host);
+    if (conf == NULL)
+      conf = find_exact_name_conf(OPER_TYPE,
+                                  source_p->localClient->auth_oper,
+                                  source_p->username, source_p->sockhost);
+    if (conf == NULL)
+    {
+      sendto_one (source_p, form_str(ERR_NOOPERHOST), me.name, source_p->name);
+      log_oper_action(LOG_FAILED_OPER_TYPE, source_p, "%s\n",
+          source_p->localClient->auth_oper);
+      return;
+    }
+
+    if (attach_conf(source_p, conf) != 0)
+    {
+      sendto_one(source_p,":%s NOTICE %s :Can't attach conf!",
+     me.name, source_p->name);   
+      failed_challenge_notice(source_p, conf->name, "can't attach conf!");
+      log_oper_action(LOG_FAILED_OPER_TYPE, source_p, "%s\n", 
+          source_p->localClient->auth_oper);
+      return;
+    }
+
+    oper_up(source_p);
+
+    ilog(L_TRACE, "OPER %s by %s!%s@%s",
+   source_p->localClient->auth_oper, source_p->name, source_p->username,
+   source_p->host);
+    log_oper_action(LOG_OPER_TYPE, source_p,
+        "%s\n", source_p->localClient->auth_oper);
+
+    MyFree(source_p->localClient->response);
+    MyFree(source_p->localClient->auth_oper);
+    source_p->localClient->response  = NULL;
+    source_p->localClient->auth_oper = NULL;
+    return;
+  }
+
+  MyFree(source_p->localClient->response);
+  MyFree(source_p->localClient->auth_oper);
+  source_p->localClient->response  = NULL;
+  source_p->localClient->auth_oper = NULL;
+
+  if ((conf = find_conf_exact(OPER_TYPE,
+            parv[1], source_p->username, source_p->host
+            )) != NULL)
+    aconf = map_to_conf(conf);
+  else if ((conf = find_conf_exact(OPER_TYPE,
+           parv[1], source_p->username,
+           source_p->sockhost)) != NULL)
+    aconf = map_to_conf(conf);
+
+  if (aconf == NULL)
+  {
     sendto_one (source_p, form_str(ERR_NOOPERHOST), me.name, source_p->name);
     conf = find_exact_name_conf(OPER_TYPE, parv[1], NULL, NULL);
     failed_challenge_notice(source_p, parv[1], (conf != NULL)
