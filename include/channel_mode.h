pcangel/ircd-hybrid

- Improve services support - Add channelmode +r
git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid-8@1167 82007160-df01-0410-b94d-b575c5fd34c7

include/channel_mode.h
@@ -57,6 +57,7 @@
 #define MODE_NOPRIVMSGS 0x0020
 #define MODE_SSLONLY    0x0040
 #define MODE_OPERONLY   0x0080
+#define MODE_REGISTERED 0x0100 /* Channel has been registered with ChanServ */
 
 /* cache flags for silence on ban */
 #define CHFL_BAN_CHECKED  0x0080

oftc/oftc-hybrid

include/channel_mode.h
@@ -62,6 +62,7 @@
 #define MODE_REGONLY    0x0200
 #define MODE_SPEAKIFREG 0x0400
 #define MODE_OPMOD      0x0800
+#define MODE_REGISTERED 0x1000 /* Channel has been registered with ChanServ */
 
 /* cache flags for silence on ban */
 #define CHFL_BAN_CHECKED  0x0080