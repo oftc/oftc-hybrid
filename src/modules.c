pcangel/ircd-hybrid

- Removed MODLOAD, MODRELOAD, MODUNLOAD and MODLIST command handlers.   They all have been replaced by the new "MODULE" command which now   deals with the LOAD/UNLOAD/RELOAD/LIST parameters.
git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid-8@1437 82007160-df01-0410-b94d-b575c5fd34c7

src/modules.c
@@ -64,31 +64,12 @@ static const char *core_module_table[] =
 static dlink_list mod_paths = { NULL, NULL, 0 };
 static dlink_list conf_modules = { NULL, NULL, 0 };
 
-static void mo_modload(struct Client *, struct Client *, int, char *[]);
-static void mo_modlist(struct Client *, struct Client *, int, char *[]);
-static void mo_modreload(struct Client *, struct Client *, int, char *[]);
-static void mo_modunload(struct Client *, struct Client *, int, char *[]);
+static void mo_module(struct Client *, struct Client *, int, char *[]);
 
-struct Message modload_msgtab = {
- "MODLOAD", 0, 0, 2, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_not_oper, m_ignore, m_ignore, mo_modload, m_ignore}
+struct Message module_msgtab = {
+ "MODULE", 0, 0, 2, 0, MFLG_SLOW, 0,
+  {m_unregistered, m_not_oper, m_ignore, m_ignore, mo_module, m_ignore}
 };
-
-struct Message modunload_msgtab = {
- "MODUNLOAD", 0, 0, 2, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_not_oper, m_ignore, m_ignore, mo_modunload, m_ignore}
-};
-
-struct Message modreload_msgtab = {
-  "MODRELOAD", 0, 0, 2, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_not_oper, m_ignore, m_ignore, mo_modreload, m_ignore}
-};
-
-struct Message modlist_msgtab = {
- "MODLIST", 0, 0, 0, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_not_oper, m_ignore, m_ignore, mo_modlist, m_ignore}
-};
-
 
 int
 modules_valid_suffix(const char *name)
@@ -203,10 +184,7 @@ modules_init(void)
     exit(0);
   }
 
-  mod_add_cmd(&modload_msgtab);
-  mod_add_cmd(&modunload_msgtab);
-  mod_add_cmd(&modreload_msgtab);
-  mod_add_cmd(&modlist_msgtab);
+  mod_add_cmd(&module_msgtab);
 }
 
 /* mod_find_path()
@@ -429,75 +407,23 @@ load_one_module(const char *path)
   return -1;
 }
 
-/* load a module .. */
+/*! \brief MODULE command handler (called by operators)
+ *
+ * \param client_p Pointer to allocated Client struct with physical connection
+ *                 to this server, i.e. with an open socket connected.
+ * \param source_p Pointer to allocated Client struct from which the message
+ *                 originally comes from.  This can be a local or remote client.
+ * \param parc     Integer holding the number of supplied arguments.
+ * \param parv     Argument vector where parv[0] .. parv[parc-1] are non-NULL
+ *                 pointers.
+ * \note Valid arguments for this command are:
+ *      - parv[0] = sender prefix
+ *      - parv[1] = action [LOAD, UNLOAD, RELOAD, LIST]
+ *      - parv[2] = module name
+ */
 static void
-mo_modload(struct Client *client_p, struct Client *source_p,
-           int parc, char *parv[])
-{
-  const char *m_bn = NULL;
-
-  if (!HasOFlag(source_p, OPER_FLAG_MODULE))
-  {
-    sendto_one(source_p, form_str(ERR_NOPRIVILEGES),
-               me.name, source_p->name);
-    return;
-  }
-
-  m_bn = libio_basename(parv[1]);
-
-  if (findmodule_byname(m_bn) != NULL)
-  {
-    sendto_one(source_p, ":%s NOTICE %s :Module %s is already loaded",
-               me.name, source_p->name, m_bn);
-    return;
-  }
-
-  load_one_module(parv[1]);
-}
-
-/* unload a module .. */
-static void
-mo_modunload(struct Client *client_p, struct Client *source_p,
-             int parc, char *parv[])
-{
-  const char *m_bn = NULL;
-  struct module *modp = NULL;
-
-  if (!HasOFlag(source_p, OPER_FLAG_MODULE))
-  {
-    sendto_one(source_p, form_str(ERR_NOPRIVILEGES),
-               me.name, source_p->name);
-    return;
-  }
-
-  m_bn = libio_basename(parv[1]);
-
-  if ((modp = findmodule_byname(m_bn)) == NULL)
-  {
-    sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
-               me.name, source_p->name, m_bn);
-    return;
-  }
-
-  if (modp->flags & MODULE_FLAG_CORE)
-  {
-    sendto_one(source_p,
-               ":%s NOTICE %s :Module %s is a core module and may not be unloaded",
-	       me.name, source_p->name, m_bn);
-    return;
-  }
-
-  if (unload_one_module(m_bn, 1) == -1)
-  {
-    sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
-               me.name, source_p->name, m_bn);
-  }
-}
-
-/* unload and load in one! */
-static void
-mo_modreload(struct Client *client_p, struct Client *source_p,
-             int parc, char *parv[])
+mo_module(struct Client *client_p, struct Client *source_p,
+          int parc, char *parv[])
 {
   const char *m_bn = NULL;
   struct module *modp = NULL;
@@ -510,87 +436,122 @@ mo_modreload(struct Client *client_p, struct Client *source_p,
     return;
   }
 
-  if (!strcmp(parv[1], "*"))
+  if (EmptyString(parv[1]))
   {
-    unsigned int modnum = 0;
-    dlink_node *ptr = NULL, *ptr_next = NULL;
+    sendto_one(source_p, form_str(ERR_NEEDMOREPARAMS),
+               me.name, source_p->name, "MODULE");
+    return;
+  }
 
-    sendto_one(source_p, ":%s NOTICE %s :Reloading all modules",
-               me.name, source_p->name);
-
-    modnum = dlink_list_length(&modules_list);
-
-    DLINK_FOREACH_SAFE(ptr, ptr_next, modules_list.head)
+  if (!irccmp(parv[1], "LOAD"))
+  {
+    if (findmodule_byname((m_bn = libio_basename(parv[2]))) != NULL)
     {
-      modp = ptr->data;
-      unload_one_module(modp->name, 0);
+      sendto_one(source_p, ":%s NOTICE %s :Module %s is already loaded",
+                 me.name, source_p->name, m_bn);
+      return;
     }
 
-    load_all_modules(0);
-    load_conf_modules();
-    load_core_modules(0);
-
-    sendto_realops_flags(UMODE_ALL, L_ALL,
-                        "Module Restart: %u modules unloaded, %u modules loaded",
-                          modnum, dlink_list_length(&modules_list));
-    ilog(LOG_TYPE_IRCD, "Module Restart: %u modules unloaded, %u modules loaded",
-         modnum, dlink_list_length(&modules_list));
+    load_one_module(parv[2]);
     return;
   }
 
-  m_bn = libio_basename(parv[1]);
-
-  if ((modp = findmodule_byname(m_bn)) == NULL)
+  if (!irccmp(parv[1], "UNLOAD"))
   {
-    sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
+    if ((modp = findmodule_byname((m_bn = libio_basename(parv[2])))) == NULL)
+    {
+      sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
+                 me.name, source_p->name, m_bn);
+      return;
+    }
+
+    if (modp->flags & MODULE_FLAG_CORE)
+    {
+      sendto_one(source_p,
+                 ":%s NOTICE %s :Module %s is a core module and may not be unloaded",
+                 me.name, source_p->name, m_bn);
+      return;
+    }
+
+    if (unload_one_module(m_bn, 1) == -1)
+      sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
+                 me.name, source_p->name, m_bn);
+    return;
+  }
+
+  if (!irccmp(parv[1], "RELOAD"))
+  {
+    if (!strcmp(parv[2], "*"))
+    {
+      unsigned int modnum = 0;
+      dlink_node *ptr = NULL, *ptr_next = NULL;
+
+      sendto_one(source_p, ":%s NOTICE %s :Reloading all modules",
+                 me.name, source_p->name);
+
+      modnum = dlink_list_length(&modules_list);
+
+      DLINK_FOREACH_SAFE(ptr, ptr_next, modules_list.head)
+      {
+        modp = ptr->data;
+        unload_one_module(modp->name, 0);
+      }
+
+      load_all_modules(0);
+      load_conf_modules();
+      load_core_modules(0);
+
+      sendto_realops_flags(UMODE_ALL, L_ALL,
+                           "Module Restart: %u modules unloaded, %u modules loaded",
+                           modnum, dlink_list_length(&modules_list));
+      ilog(LOG_TYPE_IRCD, "Module Restart: %u modules unloaded, %u modules loaded",
+           modnum, dlink_list_length(&modules_list));
+      return;
+    }
+
+    if ((modp = findmodule_byname((m_bn = libio_basename(parv[2])))) == NULL)
+    {
+      sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
                me.name, source_p->name, m_bn);
+      return;
+    }
+
+    check_core = (modp->flags & MODULE_FLAG_CORE) != 0;
+
+    if (unload_one_module(m_bn, 1) == -1)
+    {
+      sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
+                 me.name, source_p->name, m_bn);
+      return;
+    }
+
+    if ((load_one_module(parv[2]) == -1) && check_core)
+    {
+      sendto_realops_flags(UMODE_ALL, L_ALL, "Error reloading core "
+                           "module: %s: terminating ircd", parv[2]);
+      ilog(LOG_TYPE_IRCD, "Error loading core module %s: terminating ircd", parv[2]);
+      exit(0);
+    }
+
     return;
   }
 
-  check_core = (modp->flags & MODULE_FLAG_CORE) != 0;
-
-  if (unload_one_module(m_bn, 1) == -1)
+  if (!irccmp(parv[1], "LIST"))
   {
-    sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
-               me.name, source_p->name, m_bn);
-    return;
-  }
+    const dlink_node *ptr = NULL;
 
-  if ((load_one_module(parv[1]) == -1) && check_core)
-  {
-    sendto_realops_flags(UMODE_ALL, L_ALL, "Error reloading core "
-                         "module: %s: terminating ircd", parv[1]);
-    ilog(LOG_TYPE_IRCD, "Error loading core module %s: terminating ircd", parv[1]);
-    exit(0);
-  }
-}
+    DLINK_FOREACH(ptr, modules_list.head)
+    {
+      if (parc > 2 && !match(parv[2], modp->name))
+        continue;
 
-/* list modules .. */
-static void
-mo_modlist(struct Client *client_p, struct Client *source_p,
-	   int parc, char *parv[])
-{
-  const dlink_node *ptr = NULL;
+      sendto_one(source_p, form_str(RPL_MODLIST), me.name, source_p->name,
+                 modp->name, modp->handle,
+                 modp->version, (modp->flags & MODULE_FLAG_CORE) ?"(core)":"");
+    }
 
-  if (!HasOFlag(source_p, OPER_FLAG_MODULE))
-  {
-    sendto_one(source_p, form_str(ERR_NOPRIVILEGES),
+    sendto_one(source_p, form_str(RPL_ENDOFMODLIST),
                me.name, source_p->name);
     return;
   }
-
-  DLINK_FOREACH(ptr, modules_list.head)
-  {
-    const struct module *modp = ptr->data;
-
-    if (parc > 1 && !match(parv[1], modp->name))
-      continue;
-
-    sendto_one(source_p, form_str(RPL_MODLIST), me.name, source_p->name,
-               modp->name, modp->handle,
-               modp->version, (modp->flags & MODULE_FLAG_CORE) ?"(core)":"");
-  }
-
-  sendto_one(source_p, form_str(RPL_ENDOFMODLIST),
-             me.name, source_p->name);
 }

oftc/oftc-hybrid

src/modules.c
@@ -70,31 +70,12 @@ static const char *core_module_table[] =
 static dlink_list mod_paths = { NULL, NULL, 0 };
 static dlink_list conf_modules = { NULL, NULL, 0 };
 
-static void mo_modload(struct Client *, struct Client *, int, char *[]);
-static void mo_modlist(struct Client *, struct Client *, int, char *[]);
-static void mo_modreload(struct Client *, struct Client *, int, char *[]);
-static void mo_modunload(struct Client *, struct Client *, int, char *[]);
+static void mo_module(struct Client *, struct Client *, int, char *[]);
 
-struct Message modload_msgtab = {
- "MODLOAD", 0, 0, 2, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_not_oper, m_ignore, m_ignore, mo_modload, m_ignore}
+struct Message module_msgtab = {
+ "MODULE", 0, 0, 2, 0, MFLG_SLOW, 0,
+  {m_unregistered, m_not_oper, m_ignore, m_ignore, mo_module, m_ignore}
 };
-
-struct Message modunload_msgtab = {
- "MODUNLOAD", 0, 0, 2, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_not_oper, m_ignore, m_ignore, mo_modunload, m_ignore}
-};
-
-struct Message modreload_msgtab = {
-  "MODRELOAD", 0, 0, 2, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_not_oper, m_ignore, m_ignore, mo_modreload, m_ignore}
-};
-
-struct Message modlist_msgtab = {
- "MODLIST", 0, 0, 0, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_not_oper, m_ignore, m_ignore, mo_modlist, m_ignore}
-};
-
 
 int
 modules_valid_suffix(const char *name)
@@ -209,10 +190,7 @@ modules_init(void)
     exit(0);
   }
 
-  mod_add_cmd(&modload_msgtab);
-  mod_add_cmd(&modunload_msgtab);
-  mod_add_cmd(&modreload_msgtab);
-  mod_add_cmd(&modlist_msgtab);
+  mod_add_cmd(&module_msgtab);
 }
 
 /* mod_find_path()
@@ -434,75 +412,23 @@ load_one_module(const char *path)
   return -1;
 }
 
-/* load a module .. */
+/*! \brief MODULE command handler (called by operators)
+ *
+ * \param client_p Pointer to allocated Client struct with physical connection
+ *                 to this server, i.e. with an open socket connected.
+ * \param source_p Pointer to allocated Client struct from which the message
+ *                 originally comes from.  This can be a local or remote client.
+ * \param parc     Integer holding the number of supplied arguments.
+ * \param parv     Argument vector where parv[0] .. parv[parc-1] are non-NULL
+ *                 pointers.
+ * \note Valid arguments for this command are:
+ *      - parv[0] = sender prefix
+ *      - parv[1] = action [LOAD, UNLOAD, RELOAD, LIST]
+ *      - parv[2] = module name
+ */
 static void
-mo_modload(struct Client *client_p, struct Client *source_p,
-           int parc, char *parv[])
-{
-  char *m_bn;
-
-  if (!HasOFlag(source_p, OPER_FLAG_MODULE))
-  {
-    sendto_one(source_p, form_str(ERR_NOPRIVILEGES),
-               me.name, source_p->name);
-    return;
-  }
-
-  m_bn = basename(parv[1]);
-
-  if (findmodule_byname(m_bn) != NULL)
-  {
-    sendto_one(source_p, ":%s NOTICE %s :Module %s is already loaded",
-               me.name, source_p->name, m_bn);
-    return;
-  }
-
-  load_one_module(parv[1]);
-}
-
-/* unload a module .. */
-static void
-mo_modunload(struct Client *client_p, struct Client *source_p,
-             int parc, char *parv[])
-{
-  const char *m_bn = NULL;
-  struct module *modp = NULL;
-
-  if (!HasOFlag(source_p, OPER_FLAG_MODULE))
-  {
-    sendto_one(source_p, form_str(ERR_NOPRIVILEGES),
-               me.name, source_p->name);
-    return;
-  }
-
-  m_bn = basename(parv[1]);
-
-  if ((modp = findmodule_byname(m_bn)) == NULL)
-  {
-    sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
-               me.name, source_p->name, m_bn);
-    return;
-  }
-
-  if (modp->flags & MODULE_FLAG_CORE)
-  {
-    sendto_one(source_p,
-               ":%s NOTICE %s :Module %s is a core module and may not be unloaded",
-	       me.name, source_p->name, m_bn);
-    return;
-  }
-
-  if (unload_one_module(m_bn, 1) == -1)
-  {
-    sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
-               me.name, source_p->name, m_bn);
-  }
-}
-
-/* unload and load in one! */
-static void
-mo_modreload(struct Client *client_p, struct Client *source_p,
-             int parc, char *parv[])
+mo_module(struct Client *client_p, struct Client *source_p,
+          int parc, char *parv[])
 {
   const char *m_bn = NULL;
   struct module *modp = NULL;
@@ -515,85 +441,122 @@ mo_modreload(struct Client *client_p, struct Client *source_p,
     return;
   }
 
-  if (!strcmp(parv[1], "*"))
+  if (EmptyString(parv[1]))
   {
-    unsigned int modnum = 0;
-    dlink_node *ptr = NULL, *ptr_next = NULL;
+    sendto_one(source_p, form_str(ERR_NEEDMOREPARAMS),
+               me.name, source_p->name, "MODULE");
+    return;
+  }
 
-    sendto_one(source_p, ":%s NOTICE %s :Reloading all modules",
-               me.name, source_p->name);
-
-    modnum = dlink_list_length(&modules_list);
-
-    DLINK_FOREACH_SAFE(ptr, ptr_next, modules_list.head)
+  if (!irccmp(parv[1], "LOAD"))
+  {
+    if (findmodule_byname((m_bn = basename(parv[2]))) != NULL)
     {
-      modp = ptr->data;
-      unload_one_module(modp->name, 0);
+      sendto_one(source_p, ":%s NOTICE %s :Module %s is already loaded",
+                 me.name, source_p->name, m_bn);
+      return;
     }
 
-    load_all_modules(0);
-    load_conf_modules();
-    load_core_modules(0);
-
-    sendto_realops_flags(UMODE_ALL, L_ALL,
-                        "Module Restart: %u modules unloaded, %u modules loaded",
-                          modnum, dlink_list_length(&modules_list));
-    ilog(LOG_TYPE_IRCD, "Module Restart: %u modules unloaded, %u modules loaded",
-         modnum, dlink_list_length(&modules_list));
+    load_one_module(parv[2]);
     return;
   }
 
-  m_bn = basename(parv[1]);
-
-  if ((modp = findmodule_byname(m_bn)) == NULL)
+  if (!irccmp(parv[1], "UNLOAD"))
   {
-    sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
+    if ((modp = findmodule_byname((m_bn = basename(parv[2])))) == NULL)
+    {
+      sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
+                 me.name, source_p->name, m_bn);
+      return;
+    }
+
+    if (modp->flags & MODULE_FLAG_CORE)
+    {
+      sendto_one(source_p,
+                 ":%s NOTICE %s :Module %s is a core module and may not be unloaded",
+                 me.name, source_p->name, m_bn);
+      return;
+    }
+
+    if (unload_one_module(m_bn, 1) == -1)
+      sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
+                 me.name, source_p->name, m_bn);
+    return;
+  }
+
+  if (!irccmp(parv[1], "RELOAD"))
+  {
+    if (!strcmp(parv[2], "*"))
+    {
+      unsigned int modnum = 0;
+      dlink_node *ptr = NULL, *ptr_next = NULL;
+
+      sendto_one(source_p, ":%s NOTICE %s :Reloading all modules",
+                 me.name, source_p->name);
+
+      modnum = dlink_list_length(&modules_list);
+
+      DLINK_FOREACH_SAFE(ptr, ptr_next, modules_list.head)
+      {
+        modp = ptr->data;
+        unload_one_module(modp->name, 0);
+      }
+
+      load_all_modules(0);
+      load_conf_modules();
+      load_core_modules(0);
+
+      sendto_realops_flags(UMODE_ALL, L_ALL,
+                           "Module Restart: %u modules unloaded, %u modules loaded",
+                           modnum, dlink_list_length(&modules_list));
+      ilog(LOG_TYPE_IRCD, "Module Restart: %u modules unloaded, %u modules loaded",
+           modnum, dlink_list_length(&modules_list));
+      return;
+    }
+
+    if ((modp = findmodule_byname((m_bn = libio_basename(parv[2])))) == NULL)
+    {
+      sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
                me.name, source_p->name, m_bn);
+      return;
+    }
+
+    check_core = (modp->flags & MODULE_FLAG_CORE) != 0;
+
+    if (unload_one_module(m_bn, 1) == -1)
+    {
+      sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
+                 me.name, source_p->name, m_bn);
+      return;
+    }
+
+    if ((load_one_module(parv[2]) == -1) && check_core)
+    {
+      sendto_realops_flags(UMODE_ALL, L_ALL, "Error reloading core "
+                           "module: %s: terminating ircd", parv[2]);
+      ilog(LOG_TYPE_IRCD, "Error loading core module %s: terminating ircd", parv[2]);
+      exit(0);
+    }
+
     return;
   }
 
-  check_core = (modp->flags & MODULE_FLAG_CORE) != 0;
-
-  if (unload_one_module(m_bn, 1) == -1)
+  if (!irccmp(parv[1], "LIST"))
   {
-    sendto_one(source_p, ":%s NOTICE %s :Module %s is not loaded",
-               me.name, source_p->name, m_bn);
-    return;
-  }
+    const dlink_node *ptr = NULL;
 
-  if ((load_one_module(parv[1]) == -1) && check_core)
-  {
-    sendto_realops_flags(UMODE_ALL, L_ALL, "Error reloading core "
-                         "module: %s: terminating ircd", parv[1]);
-    ilog(LOG_TYPE_IRCD, "Error loading core module %s: terminating ircd", parv[1]);
-    exit(0);
-  }
-}
+    DLINK_FOREACH(ptr, modules_list.head)
+    {
+      if (parc > 2 && !match(parv[2], modp->name))
+        continue;
 
-/* list modules .. */
-static void
-mo_modlist(struct Client *client_p, struct Client *source_p,
-	   int parc, char *parv[])
-{
-  dlink_node *ptr;
-  struct module *modp;
+      sendto_one(source_p, form_str(RPL_MODLIST), me.name, source_p->name,
+                 modp->name, modp->handle,
+                 modp->version, (modp->flags & MODULE_FLAG_CORE) ?"(core)":"");
+    }
 
-  if (!HasOFlag(source_p, OPER_FLAG_MODULE))
-  {
-    sendto_one(source_p, form_str(ERR_NOPRIVILEGES),
+    sendto_one(source_p, form_str(RPL_ENDOFMODLIST),
                me.name, source_p->name);
     return;
   }
-
-  DLINK_FOREACH(ptr, modules_list.head)
-  {
-    modp = ptr->data;
-
-    sendto_one(source_p, form_str(RPL_MODLIST), me.name, source_p->name,
-               modp->name, modp->handle,
-               modp->version, (modp->flags & MODULE_FLAG_CORE) ?"(core)":"");
-  }
-
-  sendto_one(source_p, form_str(RPL_ENDOFMODLIST),
-             me.name, source_p->name);
 }