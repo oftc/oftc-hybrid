dddb692b71823992d4d1d0c620b4e0cbfd99ffc7
Stuart Walsh <stu@ipng.org.uk>
2013-06-04 18:28:13 +0100

    store a server's certfp on handshake

--- src/s_serv.c
+++ src/s_serv.c
@@ -1394,6 +1394,31 @@ ssl_server_handshake(fde_t *fd, struct Client *client_p)
     }
   }
 
+  X509 *cert;
+
+  err = SSL_get_error(client_p->localClient->fd.ssl, ret);
+  ilog(LOG_TYPE_IRCD, "SSL Error %d %s", err, ERR_error_string(err, NULL));
+
+  if ((cert = SSL_get_peer_certificate(client_p->localClient->fd.ssl)) != NULL)
+  {
+    int res = SSL_get_verify_result(client_p->localClient->fd.ssl);
+    if (res == X509_V_OK || res == X509_V_ERR_SELF_SIGNED_CERT_IN_CHAIN ||
+        res == X509_V_ERR_UNABLE_TO_VERIFY_LEAF_SIGNATURE ||
+        res == X509_V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT)
+    {
+      /* The client sent a certificate which verified OK */
+      base16_encode(client_p->certfp, sizeof(client_p->certfp),
+          (const char*)cert->sha1_hash, sizeof(cert->sha1_hash));
+    }
+    else
+    {
+      ilog(LOG_TYPE_IRCD, "Server %s!%s@%s gave bad SSL client certificate: %d",
+          client_p->name, client_p->username, client_p->host, res);
+    }
+
+    X509_free(cert);
+  }
+
   finish_ssl_server_handshake(client_p);
 }
 
