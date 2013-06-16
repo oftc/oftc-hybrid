e21577e7a4eea978fa449b43df855680c2d9fd9f
Stuart Walsh <stu@ipng.org.uk>
2013-06-16 11:58:33 +0100

    actually do certfp ts6 properly and not just say we do

--- modules/m_certfp.c
+++ modules/m_certfp.c
@@ -45,17 +45,12 @@
 static void ms_certfp(struct Client *source_p, struct Client *client_p,
                       int parc, char **parv)
 {
-  struct Client *target_p;
-
-  if ((target_p = find_person(client_p, parv[1])) == NULL)
-    return;
-
-  strlcpy(target_p->certfp, parv[1], sizeof(target_p->certfp));
+  strlcpy(source_p->certfp, parv[1], sizeof(source_p->certfp));
 
   sendto_server(client_p, CAP_TS6, NOCAPS, NOFLAGS,
-                ":%s CERTFP %s %s", ID(source_p), parv[1]);
+                ":%s CERTFP :%s", ID(source_p), parv[1]);
   sendto_server(client_p, NOCAPS, CAP_TS6, NOFLAGS,
-                ":%s CERTFP %s %s", source_p->name, parv[1]);
+                ":%s CERTFP :%s", source_p->name, parv[1]);
 }
 
 struct Message certfp_msgtab =
