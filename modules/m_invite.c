e592e630c631043fb0c4e6b57d79a35e21f09404
mutantmonkey <mutantmonkey@mutantmonkey.in>
2017-06-22 13:20:08 +0000

    Apply caller ID logic to invites as well (#52)
    * Apply caller ID logic to invites as well
    
    This change extends the  existing caller ID logic that is applied when
    either +g or +G to apply to invites as well. It can be bypassed by opers
    if the opers_bypass_callerid config option is set.
    
    * Send invite notification when caller ID is enabled
    
    Send a "is messaging or inviting you" message to the target when caller
    ID is enabled and an invite is blocked. It uses the same caller ID rate
    limiting as messages.

--- modules/m_invite.c
+++ modules/m_invite.c
@@ -185,10 +185,32 @@ m_invite(struct Client *client_p, struct Client *source_p,
 
   if (MyConnect(target_p))
   {
-    sendto_one(target_p, ":%s!%s@%s INVITE %s :%s",
-               source_p->name, source_p->username,
-               source_p->host,
-               target_p->name, chptr->chname);
+    if (!IsSetCallerId(target_p) || accept_message(source_p, target_p) ||
+        (IsOper(source_p) && (ConfigFileEntry.opers_bypass_callerid == 1)))
+    {
+      sendto_one(target_p, ":%s!%s@%s INVITE %s :%s",
+                 source_p->name, source_p->username,
+                 source_p->host,
+                 target_p->name, chptr->chname);
+    }
+    else
+    {
+      sendto_one(source_p, form_str(RPL_TARGUMODEG),
+                 me.name, source_p->name, target_p->name);
+
+      if ((target_p->localClient->last_caller_id_time +
+           ConfigFileEntry.caller_id_wait) < CurrentTime)
+      {
+        sendto_one(source_p, form_str(RPL_TARGNOTIFY),
+                   ID_or_name(&me, source_p->from),
+                   ID_or_name(source_p, source_p->from), target_p->name);
+        sendto_one(target_p, form_str(RPL_UMODEGMSG),
+                   me.name, target_p->name,
+                   get_client_name(source_p, HIDE_IP));
+
+        target_p->localClient->last_caller_id_time = CurrentTime;
+      }
+    }
 
     if (chptr->mode.mode & MODE_INVITEONLY)
     {
